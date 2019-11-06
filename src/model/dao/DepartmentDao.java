a22d1cf5fb3fdc06528e392e6578f0bfe1d56db3
Regiane <regiane.bmf@gmail.com>
2019-11-06 10:06:38 -0300

    Aula 237 DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,22 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public class DepartmentDao {
+	
+	void insert(Department obj) {
+	}
+	void update(Department obj) {
+	}
+	void deleteById(Integer id) {
+	}
+	Department findById(Integer id) {
+		return null;
+	}
+	List<Department> findAll() {
+		return null;
+	}
+
+}
---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,23 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public class SellerDao {
+	
+	void insert(Seller obj) {
+	}
+	void update(Seller obj) {
+	}
+	void deleteById(Seller id) {
+	}
+	Seller findById(Integer id) {
+		return null;
+	}
+	List<Seller> findAll() {
+		return null;
+	}
+
+}
